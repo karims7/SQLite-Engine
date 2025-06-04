bf4e06628b5a11471920dbe93526e0fde1c7defc
karims7 <karims7@my.yorku.ca>
2025-06-04 05:03:26 -0400

    Fixed Main file.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,25 @@
-public class HelloWorld {
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        if (args.length < 2) {
+            System.out.println("Missing <database path> and <command>");
+            return;
+        }
+
+        String databaseFilePath = args[0];
+        String command = args[1];
+
+        switch (command) {
+            case value:
+                
+                break;
+        
+            default:
+                break;
+        }
     }
 }
